9d61d56a1f48a133c5f2946fe95d22c4c32721f5
alexander-anderson10 <10494946@uvu.edu>
2026-07-07 17:52:44 -0700

    added signleton design pattern pseudo code

--- task_manager_app/src/project/services/add_task_service.py
+++ task_manager_app/src/project/services/add_task_service.py
@@ -8,9 +8,17 @@ class AddTaskService:
     """
 
     def __init__(self) -> None:
+        # we will pass the singleton instance or reference it here.
+        # self.task_repository = task_repository_instance
         self.task_repository = TaskRepository()
 
     def add_task(self, task: Task) -> dict:
         self.task_repository.add_task(task)
 
         return {"success": True}
+
+# 1. Create a single, shared instance of the Repository
+# task_repository_instance = TaskRepository()
+
+# 2. Create a single, shared instance of the Service
+# add_task_service_instance = AddTaskService()
