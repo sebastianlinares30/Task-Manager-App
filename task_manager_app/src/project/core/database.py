381fb6e7e447273ff59a62d2d8f78485487144b5
alexander-anderson10 <10494946@uvu.edu>
2026-07-13 07:36:35 -0700

    added db sigleton psuedo code

--- task_manager_app/src/project/core/database.py
+++ task_manager_app/src/project/core/database.py
@@ -5,9 +5,13 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATABASE_PATH = BASE_DIR / "database" / "taskapp.db"
 
+# print("Opening the global SQLite database connection...")
+# This variable is created ONCE when the app starts up
+#_global_connection = sqlite3.connect(DATABASE_PATH, check_same_thread=False)
 
 def get_connection():
     """
     Creates and returns a connection to the local SQLite database.
     """
     return sqlite3.connect(DATABASE_PATH)
+    # return _global_connection
